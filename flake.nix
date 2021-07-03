1e3f8d83d690e5bf6d37eb7b3aafc9d83c783c9a
DieracDelta <justin@restivo.me>
2021-07-03 11:09:49 -0400

    Upgrade to 21.05
    Co-authored-by: Kevin Mullins kevin@pnotequalnp.com

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
